89ba35aa38be366847890254b6c3b580febd0335
Artyom Ivanov <Artyom655@yandex.ru>
2023-12-09 20:43:54 +0300

    add themeProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { Route, Routes } from 'react-router';
-import './index.scss';
+import './styles/index.scss';
 import { Link } from 'react-router-dom';
 import { AbutPageAsync } from './components/pages/AbuotPage/AbuotPage.async';
 import { MainPageAsync } from './components/pages/MainPage/MainPageAsync.async';
 import { Suspense } from 'react';
+import { useTheme } from './theme/useTheme';
 
 const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>Toggle</button>
       <Link to={'/'}>Главная</Link>
       <Link to={'/about'}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
