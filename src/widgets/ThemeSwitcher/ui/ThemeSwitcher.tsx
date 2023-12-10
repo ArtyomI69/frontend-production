e459893b50c5dc720b212c3fcb74197feb3ec9ba
Artyom Ivanov <Artyom655@yandex.ru>
2023-12-10 21:40:02 +0300

    13

---
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -0,0 +1,26 @@
+import { Theme, useTheme } from 'app/providers/ThemeProvider';
+import { FC } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import LightIcon from 'shared/assets/theme-light.svg';
+import DarkIcon from 'shared/assets/theme-dark.svg';
+import { Button, ThemeButton } from 'shared/ui/Button/Button';
+
+interface ThemeSwitcherProps {
+  className?: string;
+}
+
+export const ThemeSwitcher: FC<ThemeSwitcherProps> = (props) => {
+  const { className } = props;
+
+  const { theme, toggleTheme } = useTheme();
+
+  return (
+    <Button
+      theme={ThemeButton.CLEAR}
+      className={classNames('', {}, [className])}
+      onClick={toggleTheme}
+    >
+      {theme === Theme.LIGHT ? <LightIcon /> : <DarkIcon />}
+    </Button>
+  );
+};
