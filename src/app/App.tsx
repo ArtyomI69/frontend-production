3491277e497a88513bfaadc784b8685ca4576069
Artyom Ivanov <Artyom655@yandex.ru>
2023-12-11 22:25:39 +0300

    14 + 15

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,14 +3,21 @@ import { classNames } from '../shared/lib/classNames/classNames';
 import { useTheme } from './providers/ThemeProvider';
 import AppRouter from './router/ui/AppRouter';
 import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
+import { Suspense } from 'react';
 
 const App = () => {
-  const { theme, toggleTheme } = useTheme();
+  const { theme } = useTheme();
 
   return (
     <div className={classNames('app', { hovered: true, selected: true }, [theme])}>
-      <Navbar />
-      <AppRouter />
+      <Suspense fallback={''}>
+        <Navbar />
+        <div className="content-page">
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
     </div>
   );
 };
