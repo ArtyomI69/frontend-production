b5856835709d3e6ae0b9096859df93e62a36442e
Artyom Ivanov <Artyom655@yandex.ru>
2024-03-07 23:35:36 +0300

    update app router

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -2,3 +2,4 @@ export { userReducer, userActions } from './model/slice/userSlice';
 export { UserSchema } from './types/user';
 export { User } from './types/user';
 export { getUserAuthData } from './model/selectors/getUserAuthData/getUserAuthData';
+export { getUserInited } from './model/selectors/getUserInited/getUserInited';
