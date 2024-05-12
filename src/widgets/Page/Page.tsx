454c8eb9127a728e20763cf112f07987afc4f634
Artyom Ivanov <Artyom655@yandex.ru>
2024-05-12 21:46:13 +0300

    react-virutalized

--- src/widgets/Page/Page.tsx
+++ src/widgets/Page/Page.tsx
@@ -16,6 +16,8 @@ interface PageProps {
   onScrollEnd?: () => void;
 }
 
+export const PAGE_ID = 'PAGE_ID';
+
 export const Page = memo((props: PageProps) => {
   const { className, children, onScrollEnd } = props;
   const wrapperRef = useRef() as MutableRefObject<HTMLDivElement>;
@@ -35,7 +37,12 @@ export const Page = memo((props: PageProps) => {
   }, 500);
 
   return (
-    <section ref={wrapperRef} className={classNames(cls.Page, {}, [className])} onScroll={onScroll}>
+    <section
+      ref={wrapperRef}
+      className={classNames(cls.Page, {}, [className])}
+      onScroll={onScroll}
+      id={PAGE_ID}
+    >
       {children}
       {onScrollEnd ? <div ref={triggerRef} className={cls.trigger} /> : null}
     </section>
